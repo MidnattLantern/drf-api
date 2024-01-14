ba8a1a6335fbf3b25a60bce3fafb89a6461950de
Alma <isakvent@outlook.com>
2024-01-15 00:39:38 +0100

    can add content if authenticated

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import Post
 
-class ProfileSerializer(serializers.ModelSerializer):
+class PostSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(
         source='owner.username'
     )
