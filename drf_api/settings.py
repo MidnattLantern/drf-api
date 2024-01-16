75b1f0aae74b0e2fdbc2edda35ad9a4c3732dcb0
Alma <isakvent@outlook.com>
2024-01-16 18:57:04 +0100

    add authentication and session security

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -26,6 +26,22 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATINO_CLASSES': [(
+        'rest_framework.authentication.SessionAuthentication'
+        if 'DEV' in os.environ
+        else 'dj_rest_auth.jtw_auth.JWTCookieAuthentication'
+    )]
+}
+
+REST_USE_JWT = True
+JWT_AUTH_SECURE = True
+JWT_AUTH_COOKIE = 'my-app-auth'
+JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
+
+REST_AUTH_SERIALIZERS = {
+    'USER_DETAILS_SERIALIZER': 'drf_api.serializers.CurrentUserSerializer'
+}
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -40,14 +56,21 @@ ALLOWED_HOSTS = []
 
 
 ''' Added apps:
-cloudinary_storage
+cloudinary storage
 cloudinary
 profiles
-rest_framework
+rest framework
 comments
 likes
 followers
 django filters
+rest framework authtoken
+dj rest auth
+django contrib sites
+allauth
+allauth account
+allauth social account
+dj rest auth registration
 '''
 
 INSTALLED_APPS = [
@@ -60,15 +83,23 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cloudinary',
     'rest_framework',
+    'django_filters',
+    'rest_framework.authtoken',
+    'dj_rest_auth',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'dj_rest_auth.registration',
 
     'profiles',
     'posts',
     'comments',
     'likes',
     'followers',
-    'django_filters'
 ]
 
+SITE_ID = 1
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
